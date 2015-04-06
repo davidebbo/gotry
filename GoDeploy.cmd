bc617529f942f310dcbc3c54abe548b4dd734b06
Xiaomin Wu <xiaowu@microsoft.com>
2015-04-06 13:49:12 -0700

    Update deployment script

--- GoDeploy.cmd
+++ GoDeploy.cmd
@@ -1,36 +1,40 @@
-@echo off
+@ECHO off
 
 IF DEFINED WEBROOT_PATH (
-    echo WEBROOT_PATH is %WEBROOT_PATH%
+    ECHO WEBROOT_PATH is %WEBROOT_PATH%
     GOTO :SETUP
 )
 
-echo set WEBROOT_PATH to D:\home\site\wwwroot
+ECHO set WEBROOT_PATH to D:\home\site\wwwroot
 SET WEBROOT_PATH=D:\home\site\wwwroot
 
 :SETUP
 SET GOROOT=%WEBROOT_PATH%\go
 SET GOPATH=%WEBROOT_PATH%\gopath
-SET GOEXE=%GOROOT%\go\bin\go.exe
+SET GOEXE=%GOROOT%\bin\go.exe
+SET FOLDERNAME=azureapp
+SET GOAZUREAPP=%WEBROOT_PATH%\gopath\src\%FOLDERNAME%
 SET extract=false
 
 IF EXIST %GOROOT% (
-    echo %GOROOT% already exist
+    ECHO %GOROOT% already exist
 ) else (
-    echo creating %GOROOT%
+    ECHO creating %GOROOT%
     mkdir %GOROOT% 
     set extract=true
 )
 
 IF EXIST %GOPATH% (
-    echo %GOPATH% already exist
+    ECHO %GOPATH% already exist
 ) else (
-    echo creating %GOPATH%\bin
+    ECHO creating %GOPATH%\bin
     mkdir "%GOPATH%\bin"
-    echo creating %GOPATH%\pkg
+    ECHO creating %GOPATH%\pkg
     mkdir "%GOPATH%\pkg"
-    echo creating %GOPATH%\src
+    ECHO creating %GOPATH%\src
     mkdir "%GOPATH%\src"
+    ECHO creating %GOAZUREAPP%
+    mkdir %GOAZUREAPP%
     set extract=true
 )
 
@@ -42,17 +46,31 @@ if %extract% == true (
 
 
 :EXTRACT
-echo extracting go.zip to %WEBROOT_PATH%
+ECHO extracting go.zip to %WEBROOT_PATH%
 unzip go.zip -d %WEBROOT_PATH%
 
 GOTO :BUILD
 
 :BUILD
-echo --------------------------------------------
-echo GOROOT: %GOROOT%
-echo GOPATH: %GOPATH%
-echo --------------------------------------------
-%GOEXE% get -u > log.txt
-%GOEXE% build gotry.go > log.txt
+ECHO --------------------------------------------
+ECHO GOROOT: %GOROOT%
+ECHO GOPATH: %GOPATH%
+ECHO GOEXE: %GOEXE%
+ECHO GOAZUREAPP: %GOAZUREAPP%
+ECHO --------------------------------------------
+ECHO copying sourc code to %GOAZUREAPP%
+CP gotry.go %GOAZUREAPP%
+CP Web.config %WEBROOT_PATH%
 
-echo DONE!
+ECHO Resolving dependencies
+CD "%GOPATH%\src"
+%GOEXE% get %FOLDERNAME%
+
+ECHO Building ...
+%GOEXE% build -o %WEBROOT_PATH%\%FOLDERNAME%.exe %FOLDERNAME%
+
+ECHO cleaning up ...
+CD %WEBROOT_PATH%
+RMDIR /S /Q %GOPATH%
+
+ECHO DONE!
